bd9d4d0e6d026c7fcc1fe9170a6f34944b8c0edd
akpejionimi <akpejionimi@yahoo.com>
2019-07-23 00:37:35 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import GuestList from './GuestList'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends Component {
+  state = {
+    guests : [
+      {
+        name: "Banks",
+        isConfirmed: false
+      },
+      {
+        name: "Marks",
+        isConfirmed: true
+      },
+      {
+        name: "Zubby",
+        isConfirmed: false 
+      }
+    ]
+  }
+  render(){
+    return (
+      <div className="AppMain">
+      <header>
+        <h1>RSVP</h1>
+        <p>A basic invite App</p>
+        <form>
+            <input type="text" value="Safia" placeholder="Invite Someone"/>
+            <button type="submit" name="submit" value="submit">Submit</button>
+        </form>
       </header>
+      <div className="main">
+        <div>
+          <h2>Invitees</h2>
+          <label>
+            <input type="checkbox"/> Hide those who haven't responded
+          </label>
+        </div>
+        <table className="counter">
+          <tbody>
+            <tr>
+              <td>Attending:</td>
+              <td>2</td>
+            </tr>
+            <tr>
+              <td>Unconfirmed:</td>
+              <td>1</td>
+            </tr>
+            <tr>
+              <td>Total:</td>
+              <td>3</td>
+            </tr>
+          </tbody>
+        </table>
+        <GuestList guests={this.state.guests}/>
+      </div>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/GuestList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import propTypes from 'prop-types'
+const GuestList = (props) =>
+    <ul>
+        {props.guests.map((guest, index) =>
+            <li>
+                <span>{guest.name}</span>
+                <label>
+                    <input type="checkbox" checked= {guest.isConfirmed}/> Confirmed
+          </label>
+                <button>edit</button>
+                <button>remove</button>
+            </li>
+        )}
+    </ul>
+
+GuestList.propTypes = {
+    guests: propTypes.array.isRequired
+}
+
+export default GuestList;

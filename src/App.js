f181682a2517b1465d76620ede88c47432215f36
akpejionimi <akpejionimi@yahoo.com>
2019-07-23 16:59:33 +0100

    created components for Guest, GuestName and GuestList

--- src/App.js
+++ src/App.js
@@ -1,61 +1,105 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import GuestList from './GuestList'
 import './App.css';
 
 class App extends Component {
   state = {
-    guests : [
+    guests: [
       {
         name: "Banks",
-        isConfirmed: false
+        isConfirmed: false,
+        isEditing: false
       },
       {
         name: "Marks",
-        isConfirmed: true
+        isConfirmed: true,
+        isEditing: false
       },
       {
         name: "Zubby",
-        isConfirmed: false 
+        isConfirmed: false,
+        isEditing: true 
       }
     ]
   }
-  render(){
+
+  toggleGuestPropertyAt = (property, indexToChange) =>
+    this.setState({
+      guests: this.state.guests.map((guest, index) => {
+        if (index === indexToChange) {
+          return {
+            ...guest,
+            [property]: !guest[property]
+          };
+        }
+        return guest;
+      })
+
+    })
+
+  toggleComfirmationAt = index =>
+    this.toggleGuestPropertyAt("isConfirmed", index);
+
+  toggleEditingAt = index =>
+    this.toggleGuestPropertyAt("isEditing", index)
+
+  setNameAt = (name, indexToChange) =>
+    this.setState({
+      guests: this.state.guests.map((guest, index) => {
+        if (index === indexToChange) {
+          return {
+            ...guest,
+            name
+          };
+        }
+        return guest;
+      })
+
+    })
+
+  getTotalInvited = () => this.state.guest.length;
+
+
+  render() {
     return (
       <div className="AppMain">
-      <header>
-        <h1>RSVP</h1>
-        <p>A basic invite App</p>
-        <form>
-            <input type="text" value="Safia" placeholder="Invite Someone"/>
+        <header>
+          <h1>RSVP</h1>
+          <p>A basic invite App</p>
+          <form>
+            <input type="text" value="Safia" placeholder="Invite Someone" />
             <button type="submit" name="submit" value="submit">Submit</button>
-        </form>
-      </header>
-      <div className="main">
-        <div>
-          <h2>Invitees</h2>
-          <label>
-            <input type="checkbox"/> Hide those who haven't responded
+          </form>
+        </header>
+        <div className="main">
+          <div>
+            <h2>Invitees</h2>
+            <label>
+              <input type="checkbox" /> Hide those who haven't responded
           </label>
+          </div>
+          <table className="counter">
+            <tbody>
+              <tr>
+                <td>Attending:</td>
+                <td>2</td>
+              </tr>
+              <tr>
+                <td>Unconfirmed:</td>
+                <td>1</td>
+              </tr>
+              <tr>
+                <td>Total:</td>
+                <td>3</td>
+              </tr>
+            </tbody>
+          </table>
+          <GuestList guests={this.state.guests}
+            toggleComfirmationAt={this.toggleComfirmationAt}
+            toggleEditingAt={this.toggleEditingAt}
+            setNameAt={this.setNameAt} />
         </div>
-        <table className="counter">
-          <tbody>
-            <tr>
-              <td>Attending:</td>
-              <td>2</td>
-            </tr>
-            <tr>
-              <td>Unconfirmed:</td>
-              <td>1</td>
-            </tr>
-            <tr>
-              <td>Total:</td>
-              <td>3</td>
-            </tr>
-          </tbody>
-        </table>
-        <GuestList guests={this.state.guests}/>
       </div>
-    </div>
     );
   }
 }
--- src/GuestList.js
+++ src/GuestList.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import propTypes from 'prop-types'
+
+import Guest from "./Guest"
 const GuestList = (props) =>
     <ul>
         {props.guests.map((guest, index) =>
-            <li>
-                <span>{guest.name}</span>
-                <label>
-                    <input type="checkbox" checked= {guest.isConfirmed}/> Confirmed
-          </label>
-                <button>edit</button>
-                <button>remove</button>
-            </li>
+            <Guest key={index}
+                name={guest.name}
+                isConfirmed={guest.isConfirmed}
+                handleConfirmation={() => props.toggleComfirmationAt(index)}
+                handleToggleEditing= {() => props.toggleEditingAt(index)}
+                setName={text => props.setNameAt(text, index)}
+            />
         )}
     </ul>
 
 GuestList.propTypes = {
-    guests: propTypes.array.isRequired
+    guests: propTypes.array.isRequired,
+    toggleComfirmationAt: propTypes.func.isRequired,
+    handleToggleEditing: propTypes.func.isRequired,
+    setNameAt: propTypes.func.isRequired
 }
 
 export default GuestList;
